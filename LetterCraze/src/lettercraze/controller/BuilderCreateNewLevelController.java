63794a497307e5efa8eaa0884c2f2950bbac41ad
evharding <jharding2014@yahoo.com>
2016-11-30 14:37:19 -0500

    Fleshed out BuilderCreateNewLevelContorller

--- LetterCraze/src/lettercraze/controller/BuilderCreateNewLevelController.java
+++ LetterCraze/src/lettercraze/controller/BuilderCreateNewLevelController.java
@@ -1,9 +1,37 @@
 package lettercraze.controller;
 
-public class BuilderCreateNewLevelController {
+import java.awt.CardLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-	public BuilderCreateNewLevelController() {
-		// TODO Auto-generated constructor stub
+import lettercraze.model.Model;
+
+/**
+ * 
+ * @author Everett Harding
+ * 
+ */
+public class BuilderCreateNewLevelController extends MouseAdapter{
+
+	/** The top level application **/
+	BuilderApplication app;
+	
+	/** the model to be changed **/
+	Model model;
+	
+	/**the layout manager, for switching "Active" views **/
+	CardLayout cardLayout;	
+	
+	public BuilderCreateNewLevelController(BuilderApplicaiton app, CardLayout cardLayout, Model model) {
+		super();
+		this.app = app;
+		this.cardLayout = cardLayout;
+		this.model = model;
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent me){
+		cardLayout.show(app.getContentPane(), "BuilderView");
 	}
 
 }
