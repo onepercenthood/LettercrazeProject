32f70966dda965e97b1290c4a8ff59949850551b
evharding <jharding2014@yahoo.com>
2016-12-12 19:15:24 -0500

    Fixed issues with builderLevelSelecterView

--- LetterCraze/src/lettercraze/view/BuilderLevelSelecterView.java
+++ LetterCraze/src/lettercraze/view/BuilderLevelSelecterView.java
@@ -15,14 +15,9 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
-import lettercraze.controller.builder.BackToMenuController;
-import lettercraze.controller.builder.EditExistingLevelController;
 import lettercraze.model.FileIO;
 import lettercraze.model.Level;
 import lettercraze.model.Model;
-import javax.swing.JButton;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class BuilderLevelSelecterView extends JPanel{
 
@@ -40,29 +35,21 @@ public class BuilderLevelSelecterView extends JPanel{
 
 	public void initialize(){
 		int numLevels = levels.size();
-<<<<<<< HEAD
 		
 		if(numLevels != 0){
 			System.out.println("Number of Levels Found: "+ numLevels);
 			setLayout(new GridLayout((int) Math.sqrt(numLevels), (int) Math.sqrt(numLevels), 10, 10));				
+			
+			JButton Menubutton = new JButton("Back To Menu");
+	    	Menubutton.addMouseListener(new BackToMenuController(parent, this));
+			add(Menubutton); 
+			
 			for(Level toLoad: levels){
 				BuilderLevelThumbnail levelThumbnail = new BuilderLevelThumbnail(toLoad, parent);	
 				add(levelThumbnail);
 			}
 		} else {
 			System.err.println("There were no levels on the disk!");
-=======
-		System.out.println("Number of Levels Found: "+ numLevels);
-		setLayout(new GridLayout((int) Math.sqrt(numLevels)+1, (int) Math.sqrt(numLevels)+1, 10, 10));
-		
-		JButton Menubutton = new JButton("Back To Menu");
-	    Menubutton.addMouseListener(new BackToMenuController(parent, this));
-		add(Menubutton);
-				
-		for(Level toLoad: levels){
-			BuilderLevelThumbnail levelThumbnail = new BuilderLevelThumbnail(toLoad, parent);	
-			add(levelThumbnail);
->>>>>>> refs/remotes/origin/development
 		}
 		
 	}
