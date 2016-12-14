c6060b27fe42d81f5db566ae0fdc4b4f9f2012f1
Jack <jharding2014@yahoo.com>
2016-12-14 11:29:19 -0500

    (probably) fixed bugs in float up tiles/float down tiles

--- LetterCraze/src/lettercraze/model/BoardFiller.java
+++ LetterCraze/src/lettercraze/model/BoardFiller.java
@@ -15,16 +15,17 @@ import lettercraze.controller.player.ThemeBuilder;
  */
 public class BoardFiller{
 
-
+	/*
 	public Square[][] floatTilesUp(Square[][] squares) {
 		// TODO Auto-generated method stub
 		for(int col = 0; col < 6; col++){
-			
+
 			for(int row = 0; row < 6; row++){
-				
+
+				//if the square is active, is missing a letter, and is not the last row...
 				if((squares[row][col].isActive) & (squares[row][col].letter == null) & (squares[row][col].row < 5)){
-					
-					// If it is not the last row and the square below it does not contain a letter
+
+					// If it is not the last row and the square below it does contains a letter
 					if(squares[row+1][col].letter != null){
 						squares[row][col].letter = squares[row+1][col].letter;
 						squares[row+1][col].setLetter(null);
@@ -52,17 +53,96 @@ public class BoardFiller{
 							squares[row][col].letter = squares[row+5][col].letter;
 							squares[row+5][col].setLetter(null);
 						}
-					
+
 					}
 				}
 			}		
 		}
 		return squares;	
+	}*/
+
+	public Square[][] floatTilesUp(Square[][] squares){
+		for(int row = 0; row < 6; row++){
+			for(int col = 0; col < 6; col ++){
+				//if the square is active, is missing a letter, and is not the last row...
+				if((squares[row][col].isActive) & (squares[row][col].letter == null)){
+					Square currentSquare = squares[row][col];
+					Square nextActive = this.getNextActiveSquareInColumn(currentSquare, squares);
+					if(nextActive != null){
+						currentSquare.setLetter(nextActive.getLetter());
+						nextActive.setLetter(null);
+					}
+				}
+			}
+		}
+		return squares;
 	}
 
+	public Square[][] floatTileDown(Square[][] squares, Word word){
+//		for(int i = word.getLetters().size()- 1; i >=0; i--){
+//			Square s = word.getLetters().get(i);
+//			pushColumnDown(squares, s);
+//			//squares[s.getRow()][s.getColumn()].setLetter(s.getLetter());
+//		}
+		ArrayList<Square> sorted = this.sortWord(word);
+		for(Square s: sorted){
+			pushColumnDown(squares, s);
+		}
+		return squares;
+	}
+
+	private ArrayList<Square> sortWord(Word word) {
+		ArrayList<Square> sortedSquares = new ArrayList<Square>();
+		//loop through the columns first
+		for(int col = 0; col < 6; col++){
+			for(int row =0; row <6; row++){
+				for(Square s: word.getLetters()){
+					//insert the squares from the current column into the new arraylist from top rows to bottom rows
+					if(s.getColumn() == col && s.getRow() == row){
+						sortedSquares.add(s);
+					}
+				}
+			}
+		}
+		return sortedSquares;
+	}
+
+	/**
+	 * for a given location (currentSquare) in squares, pushes all the letters in that column down one square
+	 * and sets the letter at currentSquare to null
+	 * @param squares
+	 * @param currentSquare
+	 */
+	private void pushColumnDown(Square[][] squares, Square topSquare){
+		int row = topSquare.getRow();
+		int col = topSquare.getColumn();
+		Letter setLetter = topSquare.getLetter();
+		for(int r = row; r < 6; r++){
+			Square currentSquare = squares[r][col];			
+			if(currentSquare.isActive()){
+				System.out.println("Shifting "+ r +", " + col);
+				//System.out.println(setLetter.getLetter() + "   " + currentSquare.getLetter().getLetter() +"   ");
+				Letter tempLetter = currentSquare.getLetter();
+				//System.out.println(setLetter.getLetter() + "   " + currentSquare.getLetter().getLetter() +"   " + tempLetter);
+				currentSquare.setLetter(setLetter);
+				//System.out.println(setLetter.getLetter() + "   " + currentSquare.getLetter().getLetter() +"   " + tempLetter);
+				setLetter = tempLetter;
+				//System.out.println(setLetter.getLetter() + "   " + currentSquare.getLetter().getLetter() +"   " + tempLetter);
+			}
+			
+		}
+		//squares[row][col].setLetter(null); //set the letter at the initial location to null
+	}
+
+	
+	/**
+	 * fills all the empty cells in the given squares[][] board with random letters
+	 * @param squares
+	 * @return
+	 */
 	public Square[][] fillEmptySquares(Square[][] squares) {
 		// TODO Auto-generated method stub
-		
+
 		for(int row = 0; row < 6; row++){
 			for(int col = 0; col < 6; col++){
 				if((squares[row][col].isActive) & (squares[row][col].letter == null)){
@@ -73,17 +153,18 @@ public class BoardFiller{
 		return squares;
 	}
 
+	/*
 	public Square[][] floatTileDown(Square[][] squares, Word word){
 		Letter temp = null;
 		Letter newTemp ;
 		Letter oldTemp ;
 		boolean found = false ;
-		
+
 		for(int col = 0; col < 6; col++){
 			for(int row = 0; row < 6; row++){
 				//System.out.println(word.getWordString());
-				
-				
+
+
 				for (Square s : word.getLetters()){
 					if((s.row == row) & (s.column == col)){
 						temp = s.letter;
@@ -92,13 +173,13 @@ public class BoardFiller{
 						break;
 					} else found = false ;
 				}
-				
+
 				if(found){
 					// case for when letter is in the 1st row
 					if((squares[row][col].isActive) & (squares[row][col].row == 0)){
 						newTemp = squares[row][col].letter;
 						squares[row][col].letter = temp;
-						
+
 						for(int z = 1; z < 6; z++){
 							if(squares[row+z][col].isActive){
 								oldTemp= newTemp;
@@ -108,11 +189,11 @@ public class BoardFiller{
 						}
 					// case for when letter is in the 2nd row	
 					}else if((squares[row][col].isActive) & (squares[row][col].row == 1)){
-						
+
 						newTemp = squares[row][col].letter;
 						squares[row][col].letter = temp;
-						
-						
+
+
 						for(int z = 1; z < 5; z++){
 							if(squares[row+z][col].isActive){
 								oldTemp= newTemp;
@@ -122,10 +203,10 @@ public class BoardFiller{
 						}
 					// case for when letter is in the 3rd row
 					}else if((squares[row][col].isActive) & (squares[row][col].row == 2)){
-					
+
 						newTemp = squares[row][col].letter;
 						squares[row][col].letter = temp;
-						
+
 						for(int z = 1; z < 4; z++){
 							if(squares[row+z][col].isActive){
 								oldTemp= newTemp;
@@ -135,10 +216,10 @@ public class BoardFiller{
 						}
 					// case for when letter is in the 4th row	
 					}else if((squares[row][col].isActive) & (squares[row][col].row == 3)){
-						
+
 						newTemp = squares[row][col].letter;
 						squares[row][col].letter = temp;
-						
+
 						for(int z = 1; z < 3; z++){
 							if(squares[row+z][col].isActive){
 								oldTemp= newTemp;
@@ -150,7 +231,7 @@ public class BoardFiller{
 					}else if((squares[row][col].isActive) & (squares[row][col].row == 4)){
 						newTemp = squares[row][col].letter;
 						squares[row][col].letter = temp;
-						
+
 						for(int z = 1; z < 2; z++){
 							if(squares[row+z][col].isActive){
 								oldTemp= newTemp;
@@ -162,18 +243,18 @@ public class BoardFiller{
 					}else if((squares[row][col].isActive) & (squares[row][col].row == 5)){
 						squares[row][col].letter = temp;
 					}
-				
+
 				}
 			}
 		}
-	
+
 		return squares;	
 	}
-
+	 */
 
 	public Square[][] initialFill(Square[][] squares) {
 		// for each square
-	
+
 		for(int row = 0; row < 6; row++){
 			for(int col = 0; col < 6; col++){
 				if(squares[row][col].isActive){
@@ -188,5 +269,14 @@ public class BoardFiller{
 		// TODO Auto-generated method stub
 		ThemeBuilder tBuild = new ThemeBuilder(bState, targetWords);		
 		return tBuild.populateSquares();
+	}
+
+	private Square getNextActiveSquareInColumn(Square currentSquare, Square[][] squares){
+		for(int row = currentSquare.getRow() + 1 ; row < 6; row ++){
+			if(squares[row][currentSquare.getColumn()].isActive() && squares[row][currentSquare.getColumn()].getLetter() != null){
+				return squares[row][currentSquare.getColumn()];
+			}
+		}
+		return null;
 	}
 }
