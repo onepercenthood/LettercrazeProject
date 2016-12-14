df25ba8a2794fc5bd7d6c29752e6015f1c38e7f5
Derek Feehrer <dfeehrer@wpi.edu>
2016-12-13 22:13:54 -0500

    Created ResetDataController to reset all 15 default levels for easiness

---
+++ LetterCraze/src/lettercraze/controller/player/ResetDataController.java
@@ -0,0 +1,28 @@
+package lettercraze.controller.player;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+public class ResetDataController implements ActionListener{
+
+	public ResetDataController (){}
+	
+	public void actionPerformed(ActionEvent act) {
+		File source = new File("default_levels_original");
+		File destination = new File("default_levels");
+		
+		try {
+		    FileUtils.copyDirectory(source, destination);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+	}
+	
+}
