8737bea0f890fc291b059326bc818710048b8bc5
Hoodie <Hoodie@130.215.11.154>
2016-12-14 23:43:29 -0500

    Made Levels 7,8,9,11,12

--- LetterCraze/src/lettercraze/view/BuilderLevelSelecterView.java
+++ LetterCraze/src/lettercraze/view/BuilderLevelSelecterView.java
@@ -56,9 +56,6 @@ public class BuilderLevelSelecterView extends JPanel{
 			parent.repaint();
 
 		} else {
-			JButton Menubutton = new JButton("Back To Menu");
-	    	Menubutton.addMouseListener(new BackToMenuController(parent, this));
-			add(Menubutton); 
 			System.err.println("There were no levels on the disk!");
 		}
 		
